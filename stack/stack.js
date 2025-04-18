4e9cf8346aeb9a8f46b871168034507571c11ab7
akashguptawebdev <akashgupta.webdev@gmail.com>
2025-04-18 12:45:43 +0530

    stack implimentation

---
+++ stack/stack.js
@@ -0,0 +1,36 @@
+class stack {
+    constructor(){
+        this.stack = [];
+    }
+
+    push(data){
+        this.stack.push(data);
+    };
+
+    pop(){
+       return this.stack.pop();
+    };
+
+    peek(){
+        return this.stack[this.stack.length-1];
+    };
+
+    isEmpty(){
+        return this.stack.length === 0;
+    }
+    print() {
+        if (this.isEmpty()) {
+          console.log("Stack is empty.");
+        } else {
+          console.log("Stack contents (top -> bottom):", [...this.stack].reverse());
+        }
+      }
+}
+
+const st = new stack();
+
+st.push(0)
+st.push(1)
+st.push(2)
+st.push(3)
+st.print()
